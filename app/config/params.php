7068496b1aee805de9d4ecfbfbbe32ed4efdd1bc
Falkort <lleclercq91@outlook.com>
2025-04-17 15:36:29 +0200

    Structure MVC de base

---
+++ app/config/params.php
@@ -0,0 +1,16 @@
+<?php
+
+//Paramètres de connexion
+
+$content="";
+//Le host de la DB ( avec la façon local de my SQL c'est le port 3306 SUR CE PC)
+define('DB_HOST',"localhost:3306");         
+
+//Le nom de la DB sur laquelle on travail
+define('DB_NAME',"photo_hunter");    
+
+//Le nom de user : root (sur MAMP)
+define('DB_USER',"root");                   
+
+//Le mot de passe : root (sur MAMP)
+define('DB_PASSWORD',"root");               
